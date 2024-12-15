a7d5a57fb2d561c00d2cd011d4546eabc38e0078
PiovesanMatteo <matteo.piovesan.1@studenti.unipd.it>
2024-12-15 22:23:42 +0100

    Create BasicCount.h

---
+++ BasicCount.h
@@ -0,0 +1,25 @@
+class BasicCount {
+private:
+    struct node {
+        int value;
+        int exp;
+        node* next;
+        node* prev;
+    };
+    int k, k_half, max_exponent, time, sum, window_size;
+    node** array_last;
+    node* start;
+    node* stop;
+    int* array_count;
+public:
+    BasicCount(float e, int n);
+    ~BasicCount();
+    void printall();
+    void insert(int value);
+    int eval();
+    int nbucket();
+private:
+    void push(int exp, int value);
+    int pop(int exp);
+    void update();
+};
